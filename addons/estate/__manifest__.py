76f3b9aeae2ae6756add4a658af83ee105c54df6
daviiderez <daviiderez@hotmail.com>
2023-09-07 18:01:57 -0500

    All okey before form view changes

--- addons/estate/__manifest__.py
+++ addons/estate/__manifest__.py
@@ -11,6 +11,7 @@
         'security/ir.model.access.csv',
 
         'views/estate_property_views.xml',
-        'views/estate_menus.xml'
+        'views/estate_menus.xml',
+        'views/estate_tree_views.xml'
     ]
 }
